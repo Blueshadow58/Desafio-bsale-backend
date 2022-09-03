fe024fad87d275a30a2f85c92a9903be7376c069
Blueshadow58 <gamonalfranco@gmail.com>
2022-09-02 23:13:22 -0400

    Allow CORS

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ const app = express();
 const port = process.env.PORT || 8000;
 const productsRouter = require("./Routes/products");
 const categoriesRouter = require("./Routes/categories");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(productsRouter);
 app.use(categoriesRouter);
